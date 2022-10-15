7aba4f18288f28528ff3530e70692d8d04cae689
gavin <gavingunapala06@gmail.com>
2022-10-15 12:40:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ead/VehicleOwnerRegister.java
@@ -0,0 +1,14 @@
+package com.example.ead;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicleOwnerRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_owner_register);
+    }
+}
